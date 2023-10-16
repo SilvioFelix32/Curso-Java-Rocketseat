21e5e84324b21f0ed0c1cc9c32936713930e3db5
Silvio Félix <silvio.felix32@hotmail.com>
2023-10-16 18:43:52 -0300

    feat - second lesson

--- todolist/src/main/java/br/com/sftech/todolist/user/UserController.java
+++ todolist/src/main/java/br/com/sftech/todolist/user/UserController.java
@@ -1,5 +1,8 @@
 package br.com.sftech.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,8 +12,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UserController {
 
+    @Autowired
+    private IUserRepository userRepository;
+
     @PostMapping("/")
-    public void Create(@RequestBody UserModel userModel) {
-        System.out.println(userModel.name);
+    public ResponseEntity Create(@RequestBody UserModel userModel) {
+        var userExists = this.userRepository.findByUserName(userModel.getUsername());
+
+        if (userExists != null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe");
+        }
+
+        var userCreated = this.userRepository.save(userModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
     }
 }
